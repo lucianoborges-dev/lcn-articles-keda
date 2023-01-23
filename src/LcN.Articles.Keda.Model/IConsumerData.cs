b5fad12c935c3d83940086d456a50ac6478af00e
Luciano Borges <lucianoborges.dev@gmail.com>
2023-01-22 22:06:49 -0300

    Added new interfaces

---
+++ src/LcN.Articles.Keda.Model/IConsumerData.cs
@@ -0,0 +1,8 @@
+﻿namespace LcN.Articles.Keda.Model;
+
+public interface IConsumerData
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public string Data { get; set; }
+}
---
+++ src/LcN.Articles.Keda.Model/IProducerData.cs
@@ -0,0 +1,8 @@
+﻿namespace LcN.Articles.Keda.Model;
+
+public interface IProducerData
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public int NumOfItems { get; set; }
+}
